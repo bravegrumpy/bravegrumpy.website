20559359de19d53b46cf2b077f58f9f4f23343f6
Robin Zollner <code@bravegrumpy.com>
2024-04-01 23:18:00 -0400

    fixed dev homepage, backlinking, and added dev

--- dev.php
+++ dev.php
@@ -11,8 +11,11 @@
         <link rel="icon" href="assets/images/BG_logo.png">
     </head>
     <body>
+        <?php include("navlinks.php") ?>
         <header>
             <h1>Welcome to the Experimental Homepage</h1>
+            <p>PHP Edition</p>
+            <p>Go check out the <a href="dev.html">HTML Edition</a></p>
         </header>
         <main>
             <section>
@@ -32,10 +35,25 @@
             </section>
             <section>
                 <h2>PHP testing/practice</h2>
+                <p>This page is <code>.html</code></p>
                 <article>
                     <?php
-                        echo "This was created inside a  php tag";
+                        echo "This was created inside a php tag";
                     ?>
+                </article>
+                <article>
+                    <div>
+                        <p>Above, I placed the following:</p>
+                        <pre>
+    &lt;?php
+        echo "This was created inside a php tag";
+    ?&gt;
+                        </pre>
+                    </div>
+                    <p>It did render. This is because the <code>.php</code> file suffix is necessary, and exists here. This is unless true I can figure out how to tell the server to treat a <code>.html</code> file as php.</p>
+                    <p>I do not feel like doing that.</p>
+                    <p>This was me trying to figure out if php works when inside of an <code>.html</code> file. </p>
+                    <p>An exact copy of this page exists as a <code>.html</code> file. This is to show where PHP does and does not work. </p>
                 </article>
             </section>
         </main>
