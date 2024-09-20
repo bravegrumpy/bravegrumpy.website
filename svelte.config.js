4528ae9792cec3c601228136112f997cb165a68f
Robin Zollner <code@bravegrumpy.com>
2024-09-20 15:45:48 -0400

    configs

--- svelte.config.js
+++ svelte.config.js
@@ -4,10 +4,13 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 
 const config = {
+    extensions: ['.svelte'],
+    compilerOptions: {},
     preprocess: vitePreprocess(),
     kit: {
         adapter: adapter()
-    }
+    },
+    onwarn: (warning, handler) => handler(warning),
 };
 
 export default config;
