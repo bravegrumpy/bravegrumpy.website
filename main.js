e5e2b7dd496c973fe2a491f9167de31bfbc1a720
Robin Zollner <code@bravegrumpy.com>
2024-09-20 13:52:34 -0400

    adding some functionality to main and index

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-alert("This is main.js")
+const app = document.getElementById('app');
+const text = "This is from the main.js file.";
+const paragraph = document.createElement('p');
+app.appendChild(paragraph);
