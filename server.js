384158528d1bc09c42f0b3f8c54242524eb23a54
Robin Zollner <code@bravegrumpy.com>
2024-10-15 13:20:19 -0400

    fixing serving, adding rat-limit, hide dev blocks

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ import  express from 'express';
 import helmet from 'helmet';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import fs from 'fs';
 
 const app = express();
+app.use(helmet());
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,12 +13,17 @@ const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 3000;
 
 const root = '/';
-
-app.use(helmet());
 app.use(express.static(path.join(__dirname, root)));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'index.html'));
+    const filePath = path.join(__dirname, req.url);
+    if ((fs.existsSync(filePath)) && fs.lstatSync(filePath).isFile()) {
+        res.sendFile(filePath);
+    } else if ((fs.existsSync(filePath)) && fs.lstatSync(filePath).isDirectory()) {
+        res.sendFile(path.resolve(path.join(path.dirname(req.url), 'index.html')));
+    } else {
+        res.sendFile(path.resolve(root, 'index.html'));
+    }
 });
 
 app.listen(PORT, HOST, () => {
