71a1250075799e7aba861a58f7934d2ec9656e01
Robin Zollner <code@bravegrumpy.com>
2024-10-20 05:14:09 -0400

    Removing everying, following blog tutorial to get reasonable defaults

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,3 @@
 import { defineConfig } from 'astro/config'
 
-import svelte from '@astrojs/svelte';
-
-export default defineConfig({
-  integrations: [svelte()]
-});
+export default defineConfig();
