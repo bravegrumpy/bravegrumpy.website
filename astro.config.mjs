e189dd5805eb55bca9aced3323b80c09c30fa5c7
Robin Zollner <code@bravegrumpy.com>
2024-10-22 19:25:02 -0400

    removing  svelte from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
 
 import preact from "@astrojs/preact";
 
 import sitemap from "@astrojs/sitemap";
-
-import svelte from "@astrojs/svelte";
 
 export default defineConfig({
     site: "https://new.bravegrumpy.com",
