6342037c97574772069e43e12199d97a6a97466b
Robin Zollner <code@bravegrumpy.com>
2024-10-24 02:31:21 -0400

    removing tutorial items, adding a svelte component, and starting to use design system

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -1,20 +1,48 @@
+<script>
+    let { size = 75} = $props();
+</script>
 
+<svelte:options runes />
 
-<div class="title">
-    <h1>BraveGrumpy</h1>
+<div class="logo">
+    <img 
+        src='/favicon.png'
+        width="{size}"
+        height="{size}"
+    />
+    <div class="title">
+        <h1>BraveGrumpy</h1>
+        <p>Welcome to my chaotic castle for creativity.</p>
+    </div>
 </div>
 
 <style lang="less">
     @import '@styles/declarations.less';
     #textStyles();
+
     h1 {
-        .fonts.logoText();
+        .fonts.logoText(@size: 5rem);
+        line-height: 1rem;
     }
 
-    iconify-icon {
-        color: blue;
-        width: 512;
-        height: 512px;
-    }
+    .logo {
+        margin-bottom: 50px;
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        flex-wrap: wrap;
+        max-width: 80%;
+        border-radius: 5px;
+        border: 3px solid mediumorchid;
 
+        .title {
+            display: flex;
+            flex-direction: column;
+            align-content: flex-start;
+            justify-content: start;
+            width: auto;
+            gap: 0;
+            border: 2px solid sandybrown;
+        }
+    }
 </style>
