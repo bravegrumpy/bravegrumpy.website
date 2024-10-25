90e4ebb332d0c2dc75abd0349d3652a78ad80aea
Robin Zollner <code@bravegrumpy.com>
2024-10-24 20:33:18 -0400

    adding clerk auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import icon from 'astro-icon';
-
 import sitemap from "@astrojs/sitemap";
-
 import svelte from "@astrojs/svelte";
-
-
 import react from "@astrojs/react";
-
-
 import tailwind from "@astrojs/tailwind";
-
+import clerk from "@clerk/astro";
+import awsAmplify from 'astro-aws-amplify';
 
 export default defineConfig({
+    output: 'server',
+    adapter: awsAmplify(),
     site: "https://new.bravegrumpy.com",
-    integrations: [icon({}), sitemap(), svelte(), react(), tailwind()],
+    integrations: [icon(), sitemap(), svelte(), react(), tailwind(), clerk()],
 });
