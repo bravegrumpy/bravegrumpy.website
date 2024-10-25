bab454f3b90ac670868af06342c0ce139ef9ce2e
Robin Zollner <code@bravegrumpy.com>
2024-10-24 22:17:20 -0400

    completely removing amplify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,8 @@ import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import awsAmplify from 'astro-aws-amplify';
 
 export default defineConfig({
-    output: 'hybrid',
-    adapter: awsAmplify(),
     site: "https://new.bravegrumpy.com",
     integrations: [icon(), sitemap(), svelte(), react(), tailwind()],
 });
