4da7e2cc00d494b36d4fe85f2600735bf267e418
Robin Zollner <code@bravegrumpy.com>
2024-10-24 20:49:08 -0400

    removing clerk auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,11 @@ import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import clerk from "@clerk/astro";
 import awsAmplify from 'astro-aws-amplify';
 
 export default defineConfig({
     output: 'server',
     adapter: awsAmplify(),
     site: "https://new.bravegrumpy.com",
-    integrations: [icon(), sitemap(), svelte(), react(), tailwind(), clerk()],
+    integrations: [icon(), sitemap(), svelte(), react(), tailwind()],
 });
