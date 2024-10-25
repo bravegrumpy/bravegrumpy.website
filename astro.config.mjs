309f8ea0de102b2b94e217aa92448a408bb55310
Robin Zollner <code@bravegrumpy.com>
2024-10-25 15:03:46 -0400

    removing tailwind artifact

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,8 @@ import icon from 'astro-icon';
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
     site: "https://new.bravegrumpy.com",
-    integrations: [icon(), sitemap(), svelte(), react(), tailwind()],
+    integrations: [icon(), sitemap(), svelte(), react()],
 });
