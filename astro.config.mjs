0df99be11556384f3e9d43d1566bf0a62f8dd6ba
Robin Zollner <code@bravegrumpy.com>
2024-10-28 17:06:52 -0400

    updating astro.config.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import icon from 'astro-icon';
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
+import awsAmplify from 'astro-aws-amplify';
 
 export default defineConfig({
+    output: 'hybrid',
+    adapter: awsAmplify(),
     site: "https://new.bravegrumpy.com",
     integrations: [icon(), sitemap(), svelte(), react()],
 });
