c59ca53ceb1a156541147809e45cc346a103bd88
Robin Zollner <code@bravegrumpy.com>
2024-10-29 18:44:06 -0400

    adding a separate navigaion file

---
+++ src/scripts/navigation.js
@@ -0,0 +1,9 @@
+export const navigation = {
+    routes: [
+        {href:"/", text:"home", className:"default"},
+        {href:"/about", text:"about", className:"default"},
+        {href:"/blog", text:"blog", className:"default"},
+        {href:"/headers", text:"headers", className:"default"},
+        {href:"/components", text:"components", className:"default"},
+    ],
+}
