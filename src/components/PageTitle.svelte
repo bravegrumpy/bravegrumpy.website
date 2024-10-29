e196b82b7f61ac6a8f4997d703da425b70f2509c
Robin Zollner <code@bravegrumpy.com>
2024-10-29 17:48:02 -0400

    turning astro component into svelte component.

---
+++ src/components/PageTitle.svelte
@@ -0,0 +1,26 @@
+<div class="pageTitle">
+    <h2>Page Title Yo</h2>
+    <p>Subtitle</p>
+</div>
+
+<style lang="less">
+    @import "../styles/declarations.less";
+
+    .pageTitle {
+        background-color: @accent2a;
+        width: 30%;
+        display: flex;
+        flex-direction: column;
+        border: @borders[element];
+        border-radius: @borderRadius;
+ 
+
+        h2 {
+            .fonts.pageTitle();
+        }
+
+        p {
+            .fonts.pageSubtitle();
+        }
+    }
+</style>
