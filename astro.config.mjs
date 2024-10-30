2f26020a1cfe57e61e7a35884fe310cdfd27ba5f
Robin Zollner <code@bravegrumpy.com>
2024-10-29 21:29:17 -0400

    starting from scratch ish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
+
 import icon from 'astro-icon';
+
 import sitemap from "@astrojs/sitemap";
 
 
@@ -9,10 +11,7 @@ import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 
 
-import seoMeta from "astro-seo-meta";
-
-
 export default defineConfig({
   site: "https://new.bravegrumpy.com",
-  integrations: [icon(), sitemap(), svelte(), react(), seoMeta()],
+  integrations: [icon(), sitemap()],
 });
