9e0f6ddf154fad5418646684cdbaafe271e723dd
Robin Zollner <code@bravegrumpy.com>
2024-10-29 21:42:37 -0400

    removing amplify backend  adding svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,10 @@ import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
 
+import svelte from "@astrojs/svelte";
+
+
 export default defineConfig({
   site: "https://new.bravegrumpy.com",
-  integrations: [sitemap(), icon()],
+  integrations: [sitemap(), icon(), svelte()],
 });
