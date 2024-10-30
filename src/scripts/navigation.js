1abdd06954894a757dedc5bceaa4bce2493a629b
Robin Zollner <code@bravegrumpy.com>
2024-10-29 23:36:54 -0400

    fixingg navingation  on header

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -1,13 +1,29 @@
 export const navigation = {
     routes: [
-        {href:"/", text:"home", className:"default"},
-        {href:"/about", text:"about", className:"default"},
-        {href:"/blog", text:"blog", className:"default"},
-        {href:"/headers", text:"headers", className:"default"},
-        {href:"/components", text:"components", className:"default"},
+        {
+            href:"/", 
+            text:"home", 
+            className: "default",
+        },
+        {
+            href:"/about", 
+            text:"about", 
+            className: "default"
+        },
+        {
+            href:"/blog", 
+            text:"blog", 
+            className: "default"
+        },
+        {
+            href:"/headers", 
+            text:"headers", 
+            className: "default"
+        },
+        {
+            href:"/components", 
+            text:"components", 
+            className: "default"
+        },
     ],
-}
-
-export async function GET({ request}) {
-    return new Response(JSON.stringify(request.url));
 }
