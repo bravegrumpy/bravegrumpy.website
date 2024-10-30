c78990f09336618cabb6905f9b3335b70e126694
Robin Zollner <code@bravegrumpy.com>
2024-10-29 20:50:05 -0400

    following 0.2.36-21 as blueprint

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,8 @@ import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 
-import node from "@astrojs/node";
 
 export default defineConfig({
   site: "https://new.bravegrumpy.com",
   integrations: [icon(), sitemap(), svelte(), react()],
-  output: "server",
-
-  adapter: node({
-    mode: "standalone",
-  }),
 });
