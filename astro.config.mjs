bad30489b0e75caf0a803cad146ff7b08c074fcd
Robin Zollner <code@bravegrumpy.com>
2024-10-29 21:04:57 -0400

    adding modules

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,10 @@ import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 
 
+import seoMeta from "astro-seo-meta";
+
+
 export default defineConfig({
   site: "https://new.bravegrumpy.com",
-  integrations: [icon(), sitemap(), svelte(), react()],
+  integrations: [icon(), sitemap(), svelte(), react(), seoMeta()],
 });
