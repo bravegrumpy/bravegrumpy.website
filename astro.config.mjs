a2951c94e7fecbd8eb55ac37854a37f4c8509a36
Robin Zollner <code@bravegrumpy.com>
2024-10-29 20:59:47 -0400

    continuing to mimic working builds

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 import sitemap from "@astrojs/sitemap";
+
+
 import svelte from "@astrojs/svelte";
+
+
 import react from "@astrojs/react";
 
 
