d61b2c747b438076f526f0d3525e6ffdcccbeb24
Robin Zollner <code@bravegrumpy.com>
2024-10-30 14:41:08 -0400

    making nav background interaceive

--- src/components/PageTitle.svelte
+++ src/components/PageTitle.svelte
@@ -4,23 +4,32 @@
 </div>
 
 <style lang="less">
-    @import "../styles/declarations.less";
+    @import "@styles/declarations.less";
 
     .pageTitle {
         background-color: @accent2a;
-        width: 30%;
+        width: 100%;
         display: flex;
         flex-direction: column;
+        align-items: flex-start;
+        justify-content: flex-start;
         border: @borders[element];
         border-radius: @borderRadius;
+        gap: 0;
  
 
         h2 {
-            .fonts.pageTitle();
+            .fonts.pageTitle(@size: 3.236rem;);
+            line-height: 2rem;
+            padding: 0;
+            margin: 1rem;
         }
 
         p {
-            .fonts.pageSubtitle();
+            .fonts.pageSubtitle(@size: 1.618rem);
+            line-height: 1rem;
+            padding: 0;
+            margin: 1rem;
         }
     }
 </style>
