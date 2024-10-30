3ad326498ec29608913bf5641784ab58baeaaec1
Robin Zollner <code@bravegrumpy.com>
2024-10-29 20:13:40 -0400

    adding node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,14 @@ import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 
+import node from "@astrojs/node";
+
 export default defineConfig({
-    site: "https://new.bravegrumpy.com",
-    integrations: [icon(), sitemap(), svelte(), react()],
+  site: "https://new.bravegrumpy.com",
+  integrations: [icon(), sitemap(), svelte(), react()],
+  output: "server",
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
