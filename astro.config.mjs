c6b54279b6fd1802502e81688ab3fce177fa2ce2
Robin Zollner <code@bravegrumpy.com>
2024-10-31 00:18:43 -0400

    adding amplify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,12 @@ import icon from "astro-icon";
 
 import svelte from "@astrojs/svelte";
 
+import awsAmplify from 'astro-aws-amplify';
+
 
 export default defineConfig({
-  site: "https://bravegrumpy.com",
-  integrations: [ sitemap(), icon(), svelte()],
+    site: "https://bravegrumpy.com",
+    integrations: [ sitemap(), icon(), svelte()],
+    adapter: awsAmplify(),
+    output: 'server',
 });
