889f726e324f72d5cf0c6030c1c66ea2166a1021
Robin Zollner <code@bravegrumpy.com>
2024-11-01 01:51:57 -0400

    adding ssr adapter, switching to ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig } from 'astro/config';
 
+import awsAmplify from "astro-aws-amplify";
+
 import sitemap from "@astrojs/sitemap";
 
 import icon from "astro-icon";
 
 import react from "@astrojs/react";
 
-
 import svelte from "@astrojs/svelte";
 
-
 export default defineConfig({
-    outDir: 'dist',
     site: "https://new.bravegrumpy.com",
     integrations: [sitemap(), icon(), react(), svelte()],
+    output: 'server',
+    adapter: awsAmplify()
 });
