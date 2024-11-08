a24fa3c4315fc15841891421e11d22369aea0728
Robin Zollner <code@bravegrumpy.com>
2024-11-08 11:24:07 -0500

    adding writing route, and fontRatio

--- src/routes/writing/+page.svelte
+++ src/routes/writing/+page.svelte
@@ -1,4 +1,14 @@
+<script>
+    let route = $state();
+</script>
 <h1>Writing Home Page</h1>
 <p>This is the home page for all of my writing.
     I have a blog and some fantasy writing. 
 </p>
+
+<h2>Going another page</h2>
+<label>
+    Slug:
+    <input type='text' bind:value={route}>
+</label>
+<p><a href='/writing/{route}'>Go to writing/{route}</a></p>
---
+++ src/routes/writing/[slug]/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    let { data } = $props();
+    let page = $state();
+</script>
+
+<h1>{data.slug}</h1>
+<p>This is the {data.slug} page</p>
+
+<h2>Go to another page</h2>
+<label>
+    Slug:
+    <input type='text' bind:value={page}>
+</label>
+<p><a href='/writing/{page}'>Go to writing/{page}</a></p>
