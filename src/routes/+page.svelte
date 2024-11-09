2c1ed6fe4485657329db30706f8a4b69ed60d24d
Robin Zollner <code@bravegrumpy.com>
2024-11-08 19:23:53 -0500

    adding a cookie

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<script>
+    let { data } = $props();
+</script>
 <h1>BraveGrumpy Home Page</h1>
-<p>Welcome to the Brave Grumpy homepage. Click on the links to explore content.</p>
+<p>{data.visited ? 'friend': 'stranger'}</p>
+<p>Welcome {data.visited ? 'back': ''} to the Brave Grumpy homepage. Click on the links to explore content.</p>
