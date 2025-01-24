fe09c6067647eb344a9e6d0d22ab91bd9e1316d7
Robin Zollner <code@bravegrumpy.com>
2025-01-24 11:05:13 -0500

    starting over to ensure non-working database is gone

--- src/routes/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-    let { data } = $props();
-</script>
-<h1>BraveGrumpy Home Page</h1>
-<p>Welcome {data.visited ? 'back': ''} to the Brave Grumpy homepage. Click on the links to explore content.</p>
