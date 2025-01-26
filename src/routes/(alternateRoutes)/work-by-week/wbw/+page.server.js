1c9b48a6f0d73f44748df8dfa0ad3a1840528f69
Robin Zollner <code@bravegrumpy.com>
2025-01-26 15:25:52 -0500

    made the redirect go to the appropriate location.

--- src/routes/(alternateRoutes)/work-by-week/wbw/+page.server.js
+++ src/routes/(alternateRoutes)/work-by-week/wbw/+page.server.js
@@ -1,6 +1,5 @@
 import { redirect } from "@sveltejs/kit";
 
 export function load() {
-  redirect(308, "/");
-  //TODO: link this to the appropriate route
+  redirect(308, "/writing/huetopia/wbw");
 }
