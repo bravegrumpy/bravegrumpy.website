0d1c85cae4b0bb21a5b2053b38793ec0c1f2286e
Robin Zollner <code@bravegrumpy.com>
2025-02-01 16:53:33 -0500

    adding meta description

--- src/routes/writing/blog/+layout.svelte
+++ src/routes/writing/blog/+layout.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <svelte:head>
-  <title>{$page.data.page.text} | Bravegrumpy</title>
+  <title> Blog | Bravegrumpy</title>
 </svelte:head>
 
 <BaseLayout colStart={2} colEnd={4} rowStart={1} sectionClass="mt-[15px]">
