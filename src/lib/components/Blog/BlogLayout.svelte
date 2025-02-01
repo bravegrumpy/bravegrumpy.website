e7e74275158a1aa9442a0d3c24b8457802a4deaa
Robin Zollner <code@bravegrumpy.com>
2025-02-01 06:00:51 -0500

    updating blog layout with new Section api

--- src/lib/components/Blog/BlogLayout.svelte
+++ src/lib/components/Blog/BlogLayout.svelte
@@ -13,10 +13,19 @@
     h6 = '',
     open = false,
     title = 'title',
-    sectionClass = ``,
-    className = `${sectionClass} section font-body-text`,
+    sectionClass = `font-bodyText`,
+    section = true,
+    lightText = `text-bravegrumpy-black`,
+    lightBackground = `bg-bravegrumpy-accent2a`,
+    lightColors = `${lightText} ${lightBackground}`,
+    darkText = `dark:text-bravegrumpy-accent2a`,
+    darkBackground = `dark:bg-bravegrumpy-black`,
+    darkColors = `${darkText} ${darkBackground}`,
+    colors = `${lightColors} ${darkColors}`,
+    className = `${sectionClass} ${colors} ${section ? 'section' : ''} font-body-text`,
     articleClass = 'centerDiv',
     articleColumn = '2 / 4',
+    sectionColumn = '2 / 3',
     // sectionColumn = '2 / 4',
     children
   } = $props();
@@ -24,7 +33,17 @@
 
 <BaseContent {h2} {h3} {h4} {h5} {h6} {open} {title}>
   <Article className={articleClass} --mainWidth={articleColumn}>
-    <Section {sectionClass} {className}>
+    <Section
+      {lightText}
+      {lightBackground}
+      {lightColors}
+      {darkText}
+      {darkBackground}
+      {darkColors}
+      {colors}
+      {className}
+      --sectionColumn={sectionColumn}
+    >
       {@render children()}
     </Section>
   </Article>
