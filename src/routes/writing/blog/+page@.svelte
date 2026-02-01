2763b7a58f822c542b527220a6a5817998852567
Robin Zollner <code@bravegrumpy.com>
2026-02-01 12:21:33 -0500

    adding breadcrumbs to all writing.

--- src/routes/writing/blog/+page@.svelte
+++ src/routes/writing/blog/+page@.svelte
@@ -2,7 +2,7 @@
   import January from './2025_01.svx';
   import December from './2024_12.svx';
 
-  import { Breadcrumb, BreadcrumbItem } from 'flowbite-svelte';
+  import { Breadcrumb } from 'flowbite-svelte';
 
   import Icon from '@iconify/svelte';
 
@@ -13,26 +13,15 @@
   import NavButton from '$lib/components/NavButton.svelte';
 
   import BC from "$lib/components/Blog/BlogBreadcrumbs.svelte"
+  import { bci, breadcrumbClass } from "$lib/components/Blog/BlogBreadcrumbs.svelte";
 </script>
 
-<!-- <Breadcrumb>
-  <BreadcrumbItem
-    href="/writing/blog"
-    class="rounded-[5px] border-2 border-solid border-current bg-solarized-base-2 px-10 py-4 text-flush-orange-800 dark:bg-affair-950 dark:text-chateau-green-600"
-  >
-    <svelte:fragment slot="icon">
-      <Icon icon="hugeicons:notebook" width="24" height="24" />
-    </svelte:fragment>
-    <p
-      class="font-navMenuDefault text-xl text-flush-orange-800 hover:scale-105 hover:hue-rotate-60 dark:text-chateau-green-500"
-    >
-      Blog
-    </p>
-  </BreadcrumbItem>
-</Breadcrumb> -->
-
 <Article --articleColumn="1/4" --articleRow="1/2">
-<BC root={true} />
+  <Breadcrumb class={breadcrumbClass}>
+    {@render bci("/", "Home", "", "hugeicons:home-07")}
+    {@render bci("/writing", "Writing", "hugeicons:books-01", "")}
+    {@render bci("/writing/blog", "Blog", "hugeicons:notebook", "")}
+  </Breadcrumb>
 </Article>
 
 <Article --articleColumn="2/3" --articleRow="2/3">
