756528941eff3edffd110987d8819875dd0257aa
Robin Zollner <code@bravegrumpy.com>
2026-02-01 04:14:13 -0500

    Adding a blog post and some other stuff, such as better breadcrumb component

---
+++ src/lib/components/Blog/BlogBreadcrumbs.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    import Icon from "@iconify/svelte";
+    import { Breadcrumb, BreadcrumbItem } from "flowbite-svelte";
+
+    let breadcrumbClass = $state(
+    'bg-solarized-base-2 border-2 border-solid border-current text-flush-orange-800  w-fit px-2 rounded-[5px] py-2 dark:bg-affair-950 dark:text-chateau-green-600 flex flex-row flex-wrap');
+
+    let breadcrumbItemContentClass = $state(
+    'font-navMenuDefault text-xl text-flush-orange-800 hover:hue-rotate-60 hover:scale-105 dark:text-chateau-green-500');
+
+    interface Props {
+        year: number;
+        blogRoot: boolean;
+        writingRoot: boolean;
+    }
+    let { year=2026, blogRoot=false, writingRoot=false }:Props = $props();
+</script>
+
+<Breadcrumb class={breadcrumbClass}>
+    {@render bci("/", "Home", "", "hugeicons:home-07")}
+    {@render bci("/writing", "Writing", "hugeicons:books-01", "")}
+    {#if !writingRoot}
+        {@render bci("/writing/blog", "Blog", "hugeicons:notebook", "")}
+        {#if !blogRoot}
+            {@render bci(`/writing/blog/${year}`, `${year}`, "", "")}
+        {/if}
+    {/if}
+</Breadcrumb>
+
+{#snippet bci(href: string, text: string, icon: string, arrow?: string)}
+<BreadcrumbItem {href}>
+    <svelte:fragment slot="icon">
+        {#if arrow===""}
+            <Icon icon="hugeicons:arrow-right-01" width="24" height="24" />
+        {:else if arrow !== null}
+            <Icon icon={arrow} width="24" height="24" />
+        {:else}
+        <span></span>
+        {/if}
+    </svelte:fragment>
+    <p class={breadcrumbItemContentClass}>
+        {#if icon !== ""}
+            <Icon {icon} width="24" height="24" class="inline mb-1" />
+        {/if}
+        {text}
+    </p>
+</BreadcrumbItem>
+{/snippet}
