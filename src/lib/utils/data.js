abc2dcf87339a3b4f2c001fdd5dfd1b1502481d7
Robin Zollner <code@bravegrumpy.com>
2026-02-10 00:58:49 -0500

    fixing another pageTite typo.

--- src/lib/utils/data.js
+++ src/lib/utils/data.js
@@ -24,7 +24,7 @@ const links = [
             id: '2-0-0',
             href: '/writing/huetopia/wbw',
             text: 'Coursera Work by Week',
-            pageTite: 'Coursera Work by Week',
+            pageTitle: 'Coursera Work by Week',
             pageSubtitle: 'Writing Excercises'
           },
           {
