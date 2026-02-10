5d2eb3aac40a6e4acadb5d8d825a744947bdbcd6
Robin Zollner <code@bravegrumpy.com>
2026-02-10 00:49:25 -0500

    adding semicolon

--- src/routes/writing/queries/+page.svelte
+++ src/routes/writing/queries/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-    import Article from "$lib/components/Article.svelte"
+    import Article from "$lib/components/Article.svelte";
     import Section from "$lib/components/Section.svelte";
     import Heading from "$lib/components/Heading.svelte";
 
-    import { page } from "$app/state"
+    import { page } from "$app/state";
 
     let users = $derived(page.data.allUsers || false);
     let todoList = $derived(page.data.todoList || false);
