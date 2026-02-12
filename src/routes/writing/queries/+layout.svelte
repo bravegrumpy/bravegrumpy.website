c87d5f60eceba91f8dfc21daf0973661fa8e5e73
Robin Zollner <code@bravegrumpy.com>
2026-02-12 15:22:33 -0500

    removing unnecessary convex in queries as its in root layout.

--- src/routes/writing/queries/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import { PUBLIC_CONVEX_URL } from "$env/static/public";
-    import { setupConvex } from "convex-svelte";
-    let { children } = $props();
-
-    setupConvex(PUBLIC_CONVEX_URL);
-</script>
-
-{@render children?.()}
