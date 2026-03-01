dbd36be1630a4e5af2cf15f85876bd6090b8147d
Robin Zollner <code@bravegrumpy.com>
2026-02-28 21:38:49 -0500

    forcing trustHost to be equal to value of AUTH_TRUST_HOST

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,8 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import Github from "@auth/sveltekit/providers/github";
+import { AUTH_TRUST_HOST } from "$env/static/private"
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
-    providers: [Github]
+    providers: [Github],
+    trustHost: Boolean(AUTH_TRUST_HOST)
 })
