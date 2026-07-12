da3fd9cdcd0f84f4a0d878d621cf775f38d69be7
Robin Zollner <code@bravegrumpy.com>
2026-07-12 13:34:09 -0400

    fixing color in media block

--- src/lib/components/List/Media.svelte
+++ src/lib/components/List/Media.svelte
@@ -32,6 +32,8 @@
     </Details>
     <Details summary="Shows" Section="Planned Shows" stl>
         <p><a href="/links/media/shows">View Show List</a></p>
+        <div class="prose prose-headings:text-current prose-ul:text-current">    
             <ShowList />
+        </div>
     </Details>
 </Section>
