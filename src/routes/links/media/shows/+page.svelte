65c1aae20944bdbba45b73d0fc3ac8508908115d
Robin Zollner <code@bravegrumpy.com>
2026-07-12 13:54:25 -0400

    moving aside before the "back"

--- src/routes/links/media/shows/+page.svelte
+++ src/routes/links/media/shows/+page.svelte
@@ -8,6 +8,10 @@
 </script>
 
 <Article --articleColumn="1/2">
+    <Aside --asideHeight="fit-content" --asideColumn="1/2">
+        <h3>How To Watch</h3>
+        <p>I typically find the show I want to watch on <a href="https://ext.to" target="_blank">Ext.TO</a>, copy the magnet link, and then use <a href="https://webtor.io" target="_blank">WebTor</a> to stream the show.</p>
+    </Aside>
     <SideNavButton href="/links" text="Back To Links"/>
 </Article>
 
@@ -16,12 +20,4 @@
     <Section sectionClass="prose !max-w-none prose-headings:text-current prose-ul:marker:text-current">
         <ShowList />
     </Section>
-</Article>
-
-<Article --articleColumn="3/4">
-    <Aside --asideHeight="fit-content" --asideColumn="1/2">
-        <h3>How To Watch</h3>
-        <p>I typically find the show I want to watch on <a href="https://ext.to" target="_blank">Ext.TO</a>, copy the magnet link, and then use <a href="https://webtor.io" target="_blank">WebTor</a> to stream the show.</p>
-        <p>However, there are many ways to achieve the same goal. I know that there are single websites that do the whole thing.</p>
-    </Aside>
 </Article>
