cdf55c4b99f833664becec99867f97e20ff9557c
Robin Zollner <code@bravegrumpy.com>
2026-07-12 13:12:28 -0400

    adding an aside  for a link to get back

--- src/routes/links/media/shows/+page.svelte
+++ src/routes/links/media/shows/+page.svelte
@@ -3,8 +3,14 @@
     import Heading from "$lib/components/Heading.svelte"
     import Article from "$lib/components/Article.svelte"
     import ShowList from "$lib/components/List/media/shows.svx"
+    import Aside from "$lib/components/Aside.svelte"
 </script>
 
+<Article --article-column="1/2">
+    <Aside --asideHeight="fit-content">
+        <p><a href="/links">Links Home</a></p>
+    </Aside>
+</Article>
 <Article --articleColumn="2/3">
     <Heading>List Of Planned Shows</Heading>
     <Section sectionClass="prose !max-w-none prose-headings:text-current">
