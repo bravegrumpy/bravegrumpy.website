7c186098b3791edf2c37e7cc74eb8d5ad9f7b013
Robin Zollner <code@bravegrumpy.com>
2026-07-12 12:49:16 -0400

    reverting Layout change

--- src/routes/links/media/shows/+page.svelte
+++ src/routes/links/media/shows/+page.svelte
@@ -6,15 +6,7 @@
     import Aside from "$lib/components/Aside.svelte";
 </script>
 
-
-<Article --articleColumn="1/2">
-    <Aside --asideHeight="fit-content">
-        <a href="/links">Return Home</a>
-    </Asice>
-</Article>
-<Article --articleColumn="2/4">
-    <Heading>List Of Planned Shows</Heading>
-    <Section sectionClass="prose">
+<Heading>List Of Planned Shows</Heading>
+<Section sectionClass="prose">
         <ShowList />
-    </Section>
-</Article>
+</Section>
