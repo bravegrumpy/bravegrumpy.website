9dca425b035edbbd24a98700215c7eaf6e52cc3f
Robin Zollner <code@bravegrumpy.com>
2026-07-12 12:49:31 -0400

    removing Aside

--- src/routes/links/media/shows/+page.svelte
+++ src/routes/links/media/shows/+page.svelte
@@ -3,7 +3,6 @@
     import Heading from "$lib/components/Heading.svelte"
     import Article from "$lib/components/Article.svelte"
     import ShowList from "$lib/components/List/media/shows.svx"
-    import Aside from "$lib/components/Aside.svelte";
 </script>
 
 <Heading>List Of Planned Shows</Heading>
