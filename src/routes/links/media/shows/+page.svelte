83d21f3c2ef66e129c8c14f344813c979a6b0483
Robin Zollner <code@bravegrumpy.com>
2026-07-12 13:24:29 -0400

    adding an article back

--- src/routes/links/media/shows/+page.svelte
+++ src/routes/links/media/shows/+page.svelte
@@ -3,13 +3,16 @@
     import Heading from "$lib/components/Heading.svelte"
     import ShowList from "$lib/components/List/media/shows.svx"
     import Aside from "$lib/components/Aside.svelte"
+    import Article from "$lib/components/Article.svelte";
 </script>
 
 <Aside --asideHeight="fit-content" --asideColumn="1/2">
     <p><a href="/links">Links Home</a></p>
 </Aside>
 
+<Article --articleColumn="2/3">
 <Heading>List Of Planned Shows</Heading>
 <Section sectionClass="prose !max-w-none prose-headings:text-current">
     <ShowList />
 </Section>
+</Article>
