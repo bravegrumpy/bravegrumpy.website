98674fbf355db1ea30ec01b183f03a18a84e9df6
Robin Zollner <code@bravegrumpy.com>
2026-07-12 12:44:22 -0400

    fixing shows page layout

--- src/routes/links/media/shows/+page.svelte
+++ src/routes/links/media/shows/+page.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
     import Section from "$lib/components/Section.svelte"
     import Heading from "$lib/components/Heading.svelte"
+    import Article from "$lib/components/Article.svelte"
     import ShowList from "$lib/components/List/media/shows.svx"
+    import Aside from "$lib/components/Aside.svelte";
 </script>
 
 
-<Heading><a href="/links">Back To Links Home</a></Heading>
-
-<Section>
-    <ShowList />
-</Section>
+<Article --articleColumn="1/2">
+    <Aside --asideHeight="fit-content">
+        <a href="/links">Return Home</a>
+    </Asice>
+</Article>
+<Article --articleColumn="2/4">
+    <Heading>List Of Planned Shows</Heading>
+    <Section>
+        <ShowList />
+    </Section>
+</Article>
