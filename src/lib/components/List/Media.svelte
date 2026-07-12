bc5ebfd4ed82eecc0f9e25288dfd17c0c4b5e40f
Robin Zollner <code@bravegrumpy.com>
2026-07-12 14:33:49 -0400

    adding padding back in

--- src/lib/components/List/Media.svelte
+++ src/lib/components/List/Media.svelte
@@ -31,6 +31,6 @@
         </LOL>
     </Details>
     <div class="w-fit mx-auto px-5 py-2">
-        <NavButton  href="/links/media/shows" text="Shows" slug="/list/media/shows" />
+        <NavButton className="px-5 py-2" href="/links/media/shows" text="Shows" slug="/list/media/shows" />
     </div>
 </Section>
