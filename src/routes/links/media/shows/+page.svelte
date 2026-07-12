47c86c6750e5e987fc4f0f471b6153f34485a3ba
Robin Zollner <code@bravegrumpy.com>
2026-07-12 13:19:06 -0400

    removing "article"removing use of Article

--- src/routes/links/media/shows/+page.svelte
+++ src/routes/links/media/shows/+page.svelte
@@ -1,19 +1,15 @@
 <script lang="ts">
     import Section from "$lib/components/Section.svelte"
     import Heading from "$lib/components/Heading.svelte"
-    import Article from "$lib/components/Article.svelte"
     import ShowList from "$lib/components/List/media/shows.svx"
     import Aside from "$lib/components/Aside.svelte"
 </script>
 
-<Article --article-column="1/2">
-    <Aside --asideHeight="fit-content">
-        <p><a href="/links">Links Home</a></p>
-    </Aside>
-</Article>
-<Article --articleColumn="2/3">
-    <Heading>List Of Planned Shows</Heading>
-    <Section sectionClass="prose !max-w-none prose-headings:text-current">
-        <ShowList />
-    </Section>
-</Article>
+<Aside --asideHeight="fit-content" --asideColumn="1/2">
+    <p><a href="/links">Links Home</a></p>
+</Aside>
+
+<Heading>List Of Planned Shows</Heading>
+<Section sectionClass="prose !max-w-none prose-headings:text-current">
+    <ShowList />
+</Section>
