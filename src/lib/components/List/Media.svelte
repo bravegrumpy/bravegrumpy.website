5cf53acf97a76b8f74415b2c4979f9beaced7359
Robin Zollner <code@bravegrumpy.com>
2026-07-12 13:57:46 -0400

    enforcing a numbered list

--- src/lib/components/List/Media.svelte
+++ src/lib/components/List/Media.svelte
@@ -34,7 +34,7 @@
         <p>
             <a href="/links/media/shows">View Formatted Show List</a>
         </p>
-        <div class="prose !max-w-none prose-headings:text-current prose-ul:marker:text-current dark:!text-bravegrumpy-accent2a">
+        <div class="prose !max-w-none prose-headings:text-current prose-ul:marker:text-current dark:!text-bravegrumpy-accent2a !list-decimal">
             <ShowList />
         </div>
     </Details>
