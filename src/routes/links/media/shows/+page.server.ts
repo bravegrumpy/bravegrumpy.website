97185ac393eb89930c36f1d37679a9bd73a9f557
Robin Zollner <code@bravegrumpy.com>
2026-08-08 18:19:12 -0400

    adding an expect-error.

--- src/routes/links/media/shows/+page.server.ts
+++ src/routes/links/media/shows/+page.server.ts
@@ -1,5 +1,6 @@
 import { redirect } from "@sveltejs/kit";
-import { type PageServerLoad } from "./$types"
+//@ts-expect-error It cannot find the './$types.d.ts' for some reason.
+import { type PageServerLoad } from './$types';
 
 export const load:PageServerLoad = () => {
     redirect(308, '/links')
