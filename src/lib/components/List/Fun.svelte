a97deb3fd20dfd3a5eb4c614abe36719f08813c0
Robin Zollner <code@bravegrumpy.com>
2026-08-17 09:45:44 -0400

    making media section in fun default to open

--- src/lib/components/List/Fun.svelte
+++ src/lib/components/List/Fun.svelte
@@ -135,7 +135,7 @@
 
 <!-- <Heading>Streaming Media</Heading> -->
 <Section>
-    <Details summary="Streaming Media" Section="Watching Media" stl>
+    <Details summary="Streaming Media" Section="Watching Media" stl open>
     <!-- <Details Summary="Watching Media" stl> -->
         <p>The easiest Way to stream anything for free, atm, is to get a Magnet Link from a torrenting site, and then stream it through a website that can stream from torrent links.</p>
         <LOL>
